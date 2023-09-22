9fc84fe1840634d46693c1aba7b134fb013b1d12
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-09-22 23:14:26 +0530

    Added job component and all jobs page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,21 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { hardhat } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { hardhat } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    hardhat
-  ],
+  [hardhat],
   [publicProvider()]
 );
 
-// NEXT_PUBLIC_PROJECT_ID="19adac522b79a2ecaffa7ae69e810ef7";
-// NEXT_PUBLIC_APP_NAME='Front Door'
-
 const { connectors } = getDefaultWallets({
-  appName: 'Front Door', 
+  appName: "Front Door",
   projectId: "19adac522b79a2ecaffa7ae69e810ef7",
   chains,
 });
-
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -39,6 +33,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </WagmiConfig>
   );
 }
-
 
 export default MyApp;
