a600dccd855c3ee38dc07b5cd3466963ad6170d0
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-10-14 10:55:27 +0530

    Fixed add job to polybase

--- components/CompanyRegister.tsx
+++ components/CompanyRegister.tsx
@@ -1,23 +1,21 @@
-import React, { useContext, useState } from "react";
-import { useRouter } from "next/router";
-import { useAccount, useContractWrite } from "wagmi";
-import { recruitmentABI, recruitmentAddress } from "../src/generated";
-import { toast } from "react-toastify";
-import usePolybase from "../hooks/usePolybase";
-import * as eth from "@polybase/eth";
+import React, { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAccount, useContractWrite } from 'wagmi';
+import { recruitmentABI, recruitmentAddress } from '../src/generated';
+import { toast } from 'react-toastify';
+import usePolybase from '../hooks/usePolybase';
+import * as eth from '@polybase/eth';
 
 const CompanyRegister = () => {
-  const { address }:any = useAccount();
-  const { registerCompany } = usePolybase(
-   async (data: string) => {
-      const sig = await eth.sign(data, address);
-      return { h: "eth-personal-sign", sig };
-    })
-  ;
+  const { address }: any = useAccount();
+  const { registerCompany } = usePolybase(async (data: string) => {
+    const sig = await eth.sign(data, address);
+    return { h: 'eth-personal-sign', sig };
+  });
   const router = useRouter();
-  const [companyName, setCompanyName] = useState("");
-  const [companyDescription, setCompanyDescription] = useState("");
-  const [companySite, setCompanySite] = useState("");
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+  const [companySite, setCompanySite] = useState('');
 
   const handleCompanyNameChange = (event: any) => {
     setCompanyName(event.target.value);
@@ -31,11 +29,10 @@ const CompanyRegister = () => {
     setCompanySite(event.target.value);
   };
 
-
   const { data, isLoading, isSuccess, writeAsync } = useContractWrite({
     abi: recruitmentABI,
     address: recruitmentAddress,
-    functionName: "registerCompany",
+    functionName: 'registerCompany',
   });
 
   const registerCompanySc = async () => {
@@ -48,9 +45,8 @@ const CompanyRegister = () => {
       ];
       await writeAsync();
       if (!isSuccess) {
-        toast.error("Company Registration failed");
-      }
-      else{
+        toast.error('Company Registration failed');
+      } else {
         if (isSuccess && !isLoading) {
           const company = await registerCompany(companyData);
           if (company.id) {
@@ -58,55 +54,56 @@ const CompanyRegister = () => {
           }
         }
       }
-     
     } catch (e) {
-     toast.error("Company Registration failed");
+      toast.error('Company Registration failed');
     }
   };
 
   return (
     <>
       <div
-        id="form"
-        className="bg-blue-50 w-[300px] md:w-[30vw]  h-[50vh] p-2  flex flex-col items-center justify-center gap-4 shadow-2xl mt-[2%]">
-        <div className="flex flex-col gap-2">
-          <span className="w-1/2 bg-blue-100 text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-white-900">
+        id='form'
+        className='mt-[2%] flex h-[50vh]  w-[300px] flex-col  items-center justify-center gap-4 bg-blue-50 p-2 shadow-2xl md:w-[30vw]'
+      >
+        <div className='flex flex-col gap-2'>
+          <span className='dark:text-white-900 mr-2 w-1/2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-white dark:bg-blue-900'>
             Company Name
           </span>
           <input
-            type="text"
+            type='text'
             value={companyName}
             onChange={handleCompanyNameChange}
-            className="border border-slate-500 rounded-lg h-[40px] w-[200px] md:w-[20vw]"
+            className='h-[40px] w-[200px] rounded-lg border border-slate-500 md:w-[20vw]'
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <span className="w-1/3 bg-blue-100 text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-white-900">
+        <div className='flex flex-col gap-2'>
+          <span className='dark:text-white-900 mr-2 w-1/3 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-white dark:bg-blue-900'>
             Description
           </span>
           <textarea
             value={companyDescription}
             onChange={handleCompanyDescriptionChange}
-            className="border border-slate-500 rounded-lg textarea h-[80px] w-[200px] md:w-[20vw]"
+            className='textarea h-[80px] w-[200px] rounded-lg border border-slate-500 md:w-[20vw]'
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <span className="w-2/4 bg-blue-100 text-white text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-white-900">
+        <div className='flex flex-col gap-2'>
+          <span className='dark:text-white-900 mr-2 w-2/4 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-white dark:bg-blue-900'>
             Company Website
           </span>
           <input
-            type="text"
+            type='text'
             value={companySite}
             onChange={handleCompanySiteChange}
-            className="border border-slate-500 rounded-lg h-[40px] w-[200px] md:w-[20vw]"
+            className='h-[40px] w-[200px] rounded-lg border border-slate-500 md:w-[20vw]'
           />
         </div>
 
         <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-[200px] md:w-[20vw]"
+          type='button'
+          className='mb-2 mr-2 w-[200px] rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:w-[20vw]'
           onClick={registerCompanySc}
-          disabled={isLoading}>
+          disabled={isLoading}
+        >
           Register
         </button>
       </div>
