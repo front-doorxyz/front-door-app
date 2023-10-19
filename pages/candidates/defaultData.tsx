b820937a27660fbc1ac9c990a1d2ba032d1ca642
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-10-19 23:02:28 +0530

    Addressed comments

---
+++ pages/candidates/defaultData.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+const candidates = [
+  {
+    id: 1,
+    walletAddress: '0x56f…ad49',
+    feedbackScore: 5,
+  },
+  {
+    id: 2,
+    walletAddress: '0x39j…ke43',
+    feedbackScore: 5,
+  },
+  {
+    id: 3,
+    walletAddress: '0x83m…jd82',
+    feedbackScore: 4,
+  },
+  {
+    id: 4,
+    walletAddress: '0x49x…jd28',
+    feedbackScore: 4,
+  },
+  {
+    id: 5,
+    walletAddress: '0x82z…le92',
+    feedbackScore: 3,
+  },
+  {
+    id: 6,
+    walletAddress: '0x01p…hq71',
+    feedbackScore: 3,
+  },
+  {
+    id: 7,
+    walletAddress: '0x45b…hz79',
+    feedbackScore: 3,
+  },
+  {
+    id: 8,
+    walletAddress: '0x82x…nk18',
+    feedbackScore: 5,
+  },
+  {
+    id: 9,
+    walletAddress: '0x71m…hi54',
+    feedbackScore: 2,
+  },
+  {
+    id: 10,
+    walletAddress: '0x90z…ji87',
+    feedbackScore: 1,
+  },
+  {
+    id: 11,
+    walletAddress: '0x77n…lo17',
+    feedbackScore: 5,
+  },
+];
+const CandidateData = () => {
+  return (
+    <tbody>
+      {candidates?.map((item: any, index: any) => (
+        <tr
+          key={item.id}
+          className={index % 2 === 0 ? 'bg-blue-200' : 'bg-blue-100'}
+        >
+          <td className='border border-blue-500 p-2 text-black'>
+            {item.walletAddress}
+          </td>
+          <td className='border border-blue-500 p-2'>{item.feedbackScore}</td>
+          <td className='border border-blue-500 p-2'>
+            <a href='#' className='text-blue-500 hover:underline'>
+              View Profile
+            </a>
+          </td>
+          <td className='border border-blue-500 p-2'>
+            <button className='rounded bg-blue-500 px-2 py-1 text-white'>
+              Contact
+            </button>
+          </td>
+          <td className='border border-blue-500 p-2'>
+            <button
+              className='rounded bg-blue-500 px-2 py-1 text-white'
+              onClick={() => toast.warning('coming soon!')}
+            >
+              Reject
+            </button>
+          </td>
+          <td className='border border-blue-500 p-2'>
+            <button
+              className='rounded bg-blue-500 px-2 py-1 text-white'
+              onClick={() => toast.warning('coming soon!')}
+            >
+              Hire
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+export default CandidateData;
---
+++ pages/candidates/index.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from 'next';
+import React from 'react';
+import Banner from '../../components/Banner';
+import { Layout } from '../../components/layout';
+import CandidateData from './defaultData';
+
+const candidateManagement: NextPage = () => {
+  return (
+    <Layout title='Profile'>
+      <Banner title='Candidate Management' />
+      <div className='mt-[2%] flex flex-col items-center justify-center pl-3 pr-3'>
+        <table className='w-full border-collapse border border-blue-500'>
+          <thead>
+            <tr className='bg-blue-500 text-white'>
+              <th className='border border-blue-500 p-2'>
+                Candidate Wallet Address
+              </th>
+              <th className='border border-blue-500 p-2'>
+                Referrer Feedback Score
+              </th>
+              <th className='border border-blue-500 p-2'>
+                Attached Profile Link
+              </th>
+              <th className='border border-blue-500 p-2'>Contact Candidate</th>
+              <th className='border border-blue-500 p-2'>Reject Candidate</th>
+              <th className='border border-blue-500 p-2'>Hire Candidate</th>
+            </tr>
+          </thead>
+          <CandidateData />
+        </table>
+      </div>
+    </Layout>
+  );
+};
+
+export default candidateManagement;
