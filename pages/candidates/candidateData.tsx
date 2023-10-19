c3f4d610b4df530637a937df85a50baa6cba5556
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-10-19 21:12:09 +0530

    removed mock data

--- pages/candidates/candidateData.tsx
+++ pages/candidates/candidateData.tsx
@@ -3,64 +3,6 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
-const data = [
-  {
-    id: 1,
-    walletAddress: '0x56f…ad49',
-    feedbackScore: 5,
-  },
-  {
-    id: 2,
-    walletAddress: '0x39j…ke43',
-    feedbackScore: 5,
-  },
-  {
-    id: 3,
-    walletAddress: '0x83m…jd82',
-    feedbackScore: 4,
-  },
-  {
-    id: 4,
-    walletAddress: '0x49x…jd28',
-    feedbackScore: 4,
-  },
-  {
-    id: 5,
-    walletAddress: '0x82z…le92',
-    feedbackScore: 3,
-  },
-  {
-    id: 6,
-    walletAddress: '0x01p…hq71',
-    feedbackScore: 3,
-  },
-  {
-    id: 7,
-    walletAddress: '0x45b…hz79',
-    feedbackScore: 3,
-  },
-  {
-    id: 8,
-    walletAddress: '0x82x…nk18',
-    feedbackScore: 5,
-  },
-  {
-    id: 9,
-    walletAddress: '0x71m…hi54',
-    feedbackScore: 2,
-  },
-  {
-    id: 10,
-    walletAddress: '0x90z…ji87',
-    feedbackScore: 1,
-  },
-  {
-    id: 11,
-    walletAddress: '0x77n…lo17',
-    feedbackScore: 5,
-  },
-];
-
 const CandidateData = () => {
   const { readJobListingById, readCandidateById } = usePolybase();
   const [jobId, setJobId] = useState('');
